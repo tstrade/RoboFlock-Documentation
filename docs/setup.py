5c7d4b8da0fbdc8214612c606b40bf93091700ca
Thomas <tstrade@buffalo.edu>
2025-12-19 14:16:15 -0500

    Added /docs/setup.py to resolve issue

---
+++ docs/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import os
+
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="RoboFlock",
+    version="0.1",
+    install_requires=requirements,
+)
